d9d4e6c79d6b1e46d112600bcc5c7524bb114424
li-guiquan <1259743436@qq.com>
2019-11-07 16:05:40 +0800

    Initial commit

---
+++ community-center/floor-manager/src/test/java/com/springcloudalibaba/floormanager/FloorManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springcloudalibaba.floormanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FloorManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ community-center/person-manager/src/test/java/com/springcloudalibaba/personmanager/PersonManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springcloudalibaba.personmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
